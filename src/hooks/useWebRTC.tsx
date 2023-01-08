766094c90b9876dc51643cec476b878c79024c64
jellymando <cicikdi@naver.com>
2023-01-08 23:44:23 +0900

    feat: 코드 정리

--- src/hooks/useWebRTC.tsx
+++ src/hooks/useWebRTC.tsx
@@ -1,16 +1,24 @@
-import React, { useRef, useCallback, useEffect } from "react";
+import React, { useRef, useMemo, useCallback, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+
+import { ROLE } from "constants/message";
 
 import useSocket from "hooks/useSocket";
 
-const signaling = new BroadcastChannel("webrtc");
-
 function useWebRTC() {
+  const signaling = new BroadcastChannel("webrtc");
   const location = useLocation();
   const { sendMessage } = useSocket();
   const pcRef = useRef<any>(null);
+  const role = useMemo(
+    () =>
+      location.pathname.replace("/", "") === ROLE.CLIENT
+        ? ROLE.CLIENT
+        : ROLE.VIEWER,
+    []
+  );
 
-  const createPeerConnection = useCallback(async (stream?: any) => {
+  const createPeerConnection = useCallback(() => {
     pcRef.current = new RTCPeerConnection();
     pcRef.current.onicecandidate = (e: any) => {
       const message = {
@@ -28,22 +36,6 @@ function useWebRTC() {
     pcRef.current.addEventListener("connectionstatechange", (event: any) => {
       console.log("connectionstatechange", event);
     });
-
-    //나중에 ROLE로 분기하기
-    if (stream) {
-      stream
-        .getTracks()
-        .forEach((track: any) => pcRef.current.addTrack(track, stream));
-    } else {
-      const $video = document.getElementById("video") as HTMLVideoElement;
-      console.log("$video", $video);
-      if ($video) {
-        pcRef.current.ontrack = (e: any) => {
-          console.log("ontrack", e);
-          $video.srcObject = e.streams[0];
-        };
-      }
-    }
   }, []);
 
   const startScreenShare = useCallback(async () => {
@@ -57,10 +49,16 @@ function useWebRTC() {
       $video.srcObject = stream;
       sendMessage({ payload: "클라이언트가 화면을 공유했습니다." });
     }
-    console.log("stream", $video, stream);
 
-    if (pcRef.current) return;
-    await createPeerConnection(stream);
+    if (!pcRef.current) {
+      createPeerConnection();
+    }
+
+    if (stream) {
+      stream
+        .getTracks()
+        .forEach((track: any) => pcRef.current.addTrack(track, stream));
+    }
 
     const offer = await pcRef.current.createOffer();
     signaling.postMessage({ type: "offer", sdp: offer.sdp });
@@ -74,27 +72,46 @@ function useWebRTC() {
     }
   }, []);
 
+  const handleOffer = useCallback(async (offer: any) => {
+    createPeerConnection();
+
+    const $video = document.getElementById("video") as HTMLVideoElement;
+    console.log("$video", $video);
+    if ($video) {
+      pcRef.current.ontrack = (e: any) => {
+        console.log("ontrack", e);
+        $video.srcObject = e.streams[0];
+      };
+    }
+
+    await pcRef.current.setRemoteDescription(offer);
+    const answer = await pcRef.current.createAnswer();
+    signaling.postMessage({ type: "answer", sdp: answer.sdp });
+    await pcRef.current.setLocalDescription(answer);
+  }, []);
+
+  const handleAnswer = useCallback(async (answer: any) => {
+    console.log("role", role);
+    await pcRef.current.setRemoteDescription(answer);
+  }, []);
+
+  const handleCandidate = useCallback(async (candidate: any) => {
+    console.log("role", role);
+    await pcRef.current.addIceCandidate(candidate.candidate ? candidate : null);
+  }, []);
+
   useEffect(() => {
     signaling.onmessage = async (e) => {
-      console.log("e.data.type", e.data.type);
+      console.log("e.data.type", e.data.type, pcRef.current);
       switch (e.data.type) {
         case "offer":
-          await createPeerConnection();
-          await pcRef.current.setRemoteDescription(e.data);
-
-          const answer = await pcRef.current.createAnswer();
-          signaling.postMessage({ type: "answer", sdp: answer.sdp });
-          await pcRef.current.setLocalDescription(answer);
+          await handleOffer(e.data);
           break;
         case "answer":
-          await pcRef.current.setRemoteDescription(e.data);
+          await handleAnswer(e.data);
           break;
         case "candidate":
-          if (!e.data.candidate) {
-            await pcRef.current.addIceCandidate(null);
-          } else {
-            await pcRef.current.addIceCandidate(e.data);
-          }
+          await handleCandidate(e.data);
           break;
         default:
           break;
